6ba0a575cfc1c5b010e31a92732b0b2eb8a5617e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-07-12 14:52:49 -0300

    removing ' syntax: no special syntax besides letters and parentheses

--- primitive-eval.lisp
+++ primitive-eval.lisp
@@ -1,54 +1,54 @@
-;eval only with primitives: quote, atom, eq,  car, cdr, cons, cond
+;eval only with primitives: quote, atom, eq,  car, cdr, cons, cond with no special syntax besides parentheses
 
 
 (defun null. (x)
-       (eq x '()))
+       (eq x (quote ())))
 
 (defun and. (x y)
        (cond (x
-               (cond (y 't)
-                     ('t '())))
-             ('t '())))
+               (cond (y (quote t))
+                     ((quote t) (quote ()))))
+             ((quote t) (quote ()))))
 
 (defun not. (x)
-       (cond (x '())
-             ('t 't)))
+       (cond (x (quote ()))
+             ((quote t) (quote t))))
 
 (defun append. (x y)
        (cond ((null. x) y)
-             ('t (cons (car x) (append. (cdr x) y)))))
+             ((quote t) (cons (car x) (append. (cdr x) y)))))
 
 (defun pair. (x y)
-       (cond ((and. (null. x) (null. y)) '())
+       (cond ((and. (null. x) (null. y)) (quote ()))
              ((and. (not. (atom x)) (not. (atom y)))
-              (cons (cons (car x) (cons (car y) '()))
+              (cons (cons (car x) (cons (car y) (quote ())))
                           (pair. (cdr x) (cdr y))))))
 
        (defun assoc. (x y)
               (cond ((eq (car (car y)) x) (car (cdr (car y))))
-                    ('t (assoc. x (cdr y)))))
+                    ((quote t) (assoc. x (cdr y)))))
 
        (defun eval. (e a)
               (cond
                 ((atom e) (assoc. e a))
                 ((atom (car e))
                  (cond
-                   ((eq (car e) 'quote) (car (cdr e)))
-                   ((eq (car e) 'atom) (atom (eval. (car (cdr e)) a)))
-                   ((eq (car e) 'eq) (eq (eval. (car (cdr e)) a)
+                   ((eq (car e) (quote quote)) (car (cdr e)))
+                   ((eq (car e) (quote atom)) (atom (eval. (car (cdr e)) a)))
+                   ((eq (car e) (quote eq)) (eq (eval. (car (cdr e)) a)
                                          (eval. (car (cdr (cdr e))) a)))
-                   ((eq (car e) 'car) (car (eval. (car (cdr e)) a)))
-                   ((eq (car e) 'cdr) (cdr (eval. (car (cdr e)) a)))
-                   ((eq (car e) 'cons) (cons (eval. (car (cdr e)) a)
+                   ((eq (car e) (quote car)) (car (eval. (car (cdr e)) a)))
+                   ((eq (car e) (quote cdr)) (cdr (eval. (car (cdr e)) a)))
+                   ((eq (car e) (quote cons)) (cons (eval. (car (cdr e)) a)
                                              (eval. (car (cdr (cdr e))) a)))
-                   ((eq (car e) 'cond) (evcon. (cdr e) a))
-                   ('t (eval. (cons (assoc. (car e) a)
+                   ((eq (car e) (quote cond)) (evcon. (cdr e) a))
+                   ((quote t) (eval. (cons (assoc. (car e) a)
                                     (cdr e))
                               a))))
-                ((eq (car (car e)) 'label)
+                ((eq (car (car e)) (quote label))
                  (eval. (cons (car (cdr (cdr (car e)))) (cdr e))
-                        (cons (cons (car (cdr (car e))) (cons (car e) '())) a)))
-                 ((eq (car (car e)) 'lambda)
+                        (cons (cons (car (cdr (car e))) (cons (car e) (quote ()))) a)))
+                 ((eq (car (car e)) (quote lambda))
                   (eval. (car (cdr (cdr (car e))))
                          (append. (pair. (car (cdr (car e))) (evlis. (cdr e) a))
                                   a)))))
@@ -56,63 +56,63 @@
               (defun evcon. (c a)
                      (cond ((eval. (car (car c)) a)
                             (eval. (car (cdr (car c))) a))
-                           ('t (evcon. (cdr c) a))))
+                           ((quote t) (evcon. (cdr c) a))))
 
               (defun evlis. (m a)
-                     (cond ((null. m) '())
-                           ('t (cons (eval. (car m) a)
+                     (cond ((null. m) (quote ()))
+                           ((quote t) (cons (eval. (car m) a)
                                      (evlis. (cdr m) a)))))
 
 ; ---- testing
 
-(eval. 'x '((x a) (y b)))
+(eval. (quote x) (quote ((x a) (y b))))
 ;A
 
-(eval. 'x '((x a) (y b)))
+(eval. (quote x) (quote ((x a) (y b))))
 ;A
 
-(eval. '(eq 'a 'a) '())
+(eval. (quote (eq (quote a) (quote a))) (quote ()))
 ;T
 
-(eval. '(cons x '(b c)) '((x a) (y b)))
+(eval. (quote (cons x (quote (b c)))) (quote ((x a) (y b))))
 ;(A B C)
 
-(eval. '(cond ((atom x) 'atom)
-              ('t 'list))
-       '((x '(a b))))
+(eval. (quote (cond ((atom x) (quote atom))
+              ((quote t) (quote list))))
+       (quote ((x (quote (a b))))))
 ;LIST
 
-(eval. '(f '(b c))
-       '((f (lambda (x) (cons 'a x)))))
+(eval. (quote (f (quote (b c))))
+       (quote ((f (lambda (x) (cons (quote a) x))))))
 ;(A B C)
 
-(eval. '((lambda (x) (cons 'a x)) '(b c))
-       '((f (lambda (x) (cons 'a x)))))
+(eval. (quote ((lambda (x) (cons (quote a) x)) (quote (b c))))
+       (quote ((f (lambda (x) (cons (quote a) x))))))
 ;(A B C)
 
-(eval. '((label firstatom (lambda (x)
+(eval. (quote ((label firstatom (lambda (x)
                             (cond ((atom x) x)
-                                  ('t (firstatom (car x))))))
-          y)
-         '((y ((a b) (c d)))))
+                                  ((quote t) (firstatom (car x))))))
+          y))
+         (quote ((y ((a b) (c d))))))
 ;A
 
-(eval. '((lambda (x)
+(eval. (quote ((lambda (x)
           (cond ((atom x) x)
-                ('t (firstatom (car x)))))
-        y)
-       '((firstatom 
+                ((quote t) (firstatom (car x)))))
+        y))
+       (quote ((firstatom 
           (label firstatom (lambda (x)
                             (cond ((atom x) x)
-                                  ('t (firstatom (car x)))))))
-      (y ((a b) (c d)))))
+                                  ((quote t) (firstatom (car x)))))))
+      (y ((a b) (c d))))))
 ;A
 
-(eval. '((lambda (x y) (cons x (cdr y))) 'a '(b c d)) '())
+(eval. (quote ((lambda (x y) (cons x (cdr y))) (quote a) (quote (b c d)))) (quote ()))
 ;(A C D)
 
 
-(eval. '(cons x (cdr y)) '((x a) (y (b c d))))
+(eval. (quote (cons x (cdr y))) (quote ((x a) (y (b c d)))))
 ;(A C D)
 
 
